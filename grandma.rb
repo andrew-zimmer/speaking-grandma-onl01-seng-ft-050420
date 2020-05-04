50a189a7e16406361e79ccc126c4be9252ddd169
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-04 20:24:44 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,6 +4,8 @@ def speak_to_grandma(talk)
     return'I LOVE YOU TOO PUMPKIN!'
   elsif talk != talk.upcase
     return "HUH?! SPEAK UP, SONNY!"
+  elsif talk === talk.upcase
+    return "NO, NOT SINCE 1938!"
   end
 end
 # Whatever you say to grandma, she should respond with

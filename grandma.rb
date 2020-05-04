a6183176717fc32e4f0f168593c2c147effe2195
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-04 20:19:37 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(talk)
   if talk == "I LOVE YOU GRANDMA!"
     return'I LOVE YOU TOO PUMPKIN!'
-  else if talk != talk.upcase
+  else if talk != talk.upcase || talk == talk.downcase
     return "HUH?! SPEAK UP, SONNY!"
   end
 end

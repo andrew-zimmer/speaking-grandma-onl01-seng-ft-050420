c0d19ca23e4d055de4dd1d27341b045b669e860f
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-04 20:16:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,6 +2,7 @@
 def speak_to_grandma(talk)
   if talk == "I LOVE YOU GRANDMA!"
     puts'I LOVE YOU TOO PUMPKIN!'
+  end
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
